fc9127e1b7d9cd76200fa1fe8f5c5ee6fbb2cc98
rybokot <monika.trznadel@student.uj.edu.pl>
2025-04-19 13:14:32 +0200

    fish

---
+++ 2023/02/script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+file="list.txt"
+> file.txt
+
+echo '<!DOCTYPE html>
+<html>
+<head>
+
+    <link rel="stylesheet" href="../styles.css">
+
+</head>
+<body>
+<dl>
+' >>file.txt
+
+while read line; do
+	name=$(echo "$line" | sed -n 's/^\(.*\)\February.*/\1/p')
+	date=$(echo "$line" | sed -n 's/.*\February\(.*\)/February\1/p')
+        day=$(echo "$date" | sed -n 's/.* \([0-9]*\),.*/\1/p')
+	echo "<dt> The fish of the day $date </dt>">> file.txt
+	echo "<dd> $name </dd>" >> file.txt
+	echo "<dd> <img src=$day.jpg> </dd> " >> file.txt
+	echo "<br>" >>file.txt 
+ done <"$file" 
+
+
+ echo '</dl>
+<a href=../../index.html>Home</a>
+</body>
+' >>file.txt
